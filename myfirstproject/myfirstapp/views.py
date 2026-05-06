a0ff27f4f01b093c83fefd08396e64589f2bd5b0
Alexandre KIENTZ <alexkientz6@gmail.com>
2026-05-06 14:31:48 +0200

    first commit

--- myfirstproject/myfirstapp/views.py
+++ myfirstproject/myfirstapp/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'myfirstapp/index.html')
+
+def formulaire(request):
+    return render(request, 'myfirstapp/formulaire.html')
+
+def bonjour(request):
+    nom=request.GET.get('nom')
+    return render(request, 'myfirstapp/bonjour.html', {'nom':nom})
